1b7a953d77ea2fa4efa795a0a0f2729fa6757f77
Sean Skaar <sean.skaar@aa.com>
2013-08-21 09:02:10 -0500

    Removed regular index and rerouted it to my own.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,32 +2,46 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path()
       page.should have_selector('h1', :text =>'Sample App')
 		end
 		it "should have the right title" do
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+			visit root_path
+			page.should have_selector('title', :text => 
+				"Ruby on Rails Tutorial Sample App")
 		end
 	end
 	describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path()
       page.should have_selector('h1', :text =>'Help')
 		end
 		it "should have the right title" do
-			visit '/static_pages/help'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+			visit help_path()
+			page.should have_selector('title', :text => 
+				"Ruby on Rails Tutorial Sample App | Help")
 		end 
 	end
 	describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path()
       page.should have_selector('h1', :text =>'About Us')
 		end
 		it "should have the right title" do
-			visit '/static_pages/about'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+			visit about_path()
+			page.should have_selector('title', :text => 
+				"Ruby on Rails Tutorial Sample App | About")
+		end
+	end
+	describe "Contact Page" do
+		it "should have the content 'Contact'" do
+      visit contact_path()
+      page.should have_selector('h1', :text =>'Contact')
+		end
+		it "should have the right title" do
+			visit contact_path()
+			page.should have_selector('title', :text => 
+				"Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
