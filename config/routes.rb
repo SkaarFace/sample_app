3c1e9808284b086fab34882f227c4dc4f8eadbed
Sean Skaar <sean.skaar@aa.com>
2013-08-27 15:46:57 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
   match '/signup', to: 'users#new', as: :signup
+  match '/signin', to: 'sessions#new', as: :signin
+  match '/signout', to: 'sessions#destroy', via: :delete, as: :signout
   match '/help', to: 'static_pages#help', as: :help
   match '/about', to: 'static_pages#about', as: :about
   match '/contact', to: 'static_pages#contact', as: :contact
